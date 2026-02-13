87cb4a2083c35ed34c6086190c4d4a07620949f7
Kgotso-Koete <kgkoete@gmail.com>
2026-02-13 21:13:57 +0200

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -12,10 +12,28 @@ class HomePageTest(TestCase):
         self.assertContains(response, '<form method="POST">')
         self.assertContains(response, '<input name="item_text"')  
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+
     def test_can_save_a_POST_request(self):
+        self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertContains(response, "A new list item")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertRedirects(response, "/")
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
 
     
 class ItemModelTest(TestCase):
@@ -35,3 +53,10 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
         self.assertEqual(second_saved_item.text, "Item the second")
+
+    
+    def test_can_save_multiple_items(self):
+        self.client.post("/", data={"item_text": "first item"})
+        response = self.client.post("/", data={"item_text": "second item"})
+        self.assertContains(response, "first item")
+        self.assertContains(response, "second item")
