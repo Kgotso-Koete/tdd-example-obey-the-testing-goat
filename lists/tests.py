1854c79edfe0eb6abddcff4ac7ae89606a3c02df
Kgotso-Koete <kgkoete@gmail.com>
2026-02-19 16:05:55 +0200

    chapter 7 update list routes

--- lists/tests.py
+++ lists/tests.py
@@ -9,7 +9,7 @@ class HomePageTest(TestCase):
 
     def test_renders_input_form(self):
         response = self.client.get("/")
-        self.assertContains(response, '<form method="POST" action="/">')
+        self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, '<input name="item_text"')  
 
     def test_can_save_a_POST_request(self):
@@ -26,6 +26,16 @@ class HomePageTest(TestCase):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
 
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.get()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -34,7 +44,7 @@ class ListViewTest(TestCase):
         
     def test_renders_input_form(self):
         response = self.client.get("/lists/the-only-list-in-the-world/")
-        self.assertContains(response, 'form method="POST" action="/">')
+        self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, '<input name="item_text"')
 
     def test_displays_all_list_items(self):
